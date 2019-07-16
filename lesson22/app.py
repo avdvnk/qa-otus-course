cbc17f93a7d30c93c523d48aaea6e2fd0fb3ab91
avdvnk <a.vdvnk@mail.ru>
2019-07-16 13:55:23 +0300

    Lesson 22. Part1 fixed

--- lesson22/app.py
+++ lesson22/app.py
@@ -2,10 +2,11 @@ import socket
 
 
 def send_request(method, host, port, url, headers, body=""):
-    request = "{} {}/{} HTTP/1.1\n".format(method, host, url)
+    request = "{} {} HTTP/1.1\r\n".format(method, url)
     for key, value in headers.items():
-        request += "{}: {}\n".format(key, value)
-    request += "\n{}".format(body)
+        request += "{}: {}\r\n".format(key, value)
+    request += "\r\n"
+    request += body
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
@@ -14,20 +15,19 @@ def send_request(method, host, port, url, headers, body=""):
 
 
 def get_response(my_socket):
-    result = ""
-    part = my_socket.recv(4096)
-    while len(part) > 0:
-        result += part
-        part = my_socket.recv(4096)
-    return result
+    result = b""
+    buffer = my_socket.recv(4096)
+    while len(buffer) > 0:
+        result += buffer
+        buffer = my_socket.recv(4096)
+    return result.decode()
 
 
 if __name__ == '__main__':
     req_method = "GET"
     req_host = "demo23.opencart.pro"
     req_port = 80
-    req_url = "admin/"
-    req_headers = {"Host": "demo23.opencart.pro",
-                   "Connection": "keep-alive"}
+    req_url = "/admin/"
+    req_headers = {"Host": "demo23.opencart.pro"}
     s = send_request(req_method, req_host, req_port, req_url, req_headers)
     print(get_response(s))
